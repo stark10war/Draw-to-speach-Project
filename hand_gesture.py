18e90797798c3e1a0728909ef722170fbd64699c
stark10war <shashank.tanwar32@gmail.com>
2019-02-19 19:57:25 +0530

    new functionality added

--- hand_gesture.py
+++ hand_gesture.py
@@ -5,7 +5,8 @@ Created on Tue Feb 12 13:29:06 2019
 @author: Shashank
 """
 import numpy as np
-import pandas as pd
+import keras
+import keyboard
 import cv2 
 import os
 os.chdir('D:/Python practice/Hand Gesture')
@@ -19,8 +20,8 @@ upper = np.array([70, 255,255])
 paint = np.zeros((480,640,1), np.uint8)
 
 old_pos = (0,0)
-
-while True:s
+img_count = 1 
+while True:
     ret, img = cap.read()
     img =  cv2.flip(img,1)
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -32,9 +33,9 @@ while True:s
     rect_len =int(img.shape[1]/4)*2
     rect_height =int(img.shape[0]/4)*2
     cv2.rectangle(img, (rect_x,rect_y), (rect_x+rect_len, rect_y+rect_height),(0,0,255),2)
-    
+    cv2.putText(img,'Draw here: ', (rect_x,rect_y), cv2.FONT_HERSHEY_SIMPLEX,1, (255,0,0), 2)
     contrs,h =cv2.findContours(img_blur1.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)#Finding conturs
-    
+    canvas = paint[rect_y:rect_y+rect_height,rect_x:rect_x+rect_len]
     if len(contrs)==1:
         for i in range(len(contrs)):
             x,y,w,h = cv2.boundingRect(contrs[i]) #geting rectamgle cordinates
@@ -44,19 +45,33 @@ while True:s
             diff_y = abs(old_pos[1] - new_pos[1])
             diff = (diff_x,diff_y)
             cv2.circle(img,new_pos, 5, (0,0,255),-1)
-            
             #Drawing line between old frame center and current frame center
-            if old_pos != (0,0) and diff<(50,50) :
-                cv2.line(paint,old_pos,new_pos,(255,255,255),5)
-                cv2.line(img,old_pos,new_pos,(255,255,255),5)
+            if old_pos != (0,0) and diff<(50,50) and keyboard.is_pressed('shift') :
+                cv2.line(paint,old_pos,new_pos,(255,255,255),5)   
             old_pos = new_pos
             
-    canvas = paint[rect_y:rect_y+rect_height,rect_x:rect_x+rect_len]
+    model = keras.models.load_model("keras_alphanumeric.mod")
+    canvas1 = cv2.resize(canvas, (32,32))
+    canvas1 = canvas1.reshape((1,32,32,1))
+    
+    prediction =model.predict(canvas1)
+        
+ # saving text images       
+#    if keyboard.is_pressed('shift') == False and canvas.sum() > 255:
+ #       cv2.imwrite('canvas'+str(img_count)+ '.jpg', canvas)
+  #      img_count = img_count+1
+  
+
+ #clearing the canvas
+    if keyboard.is_pressed('c'):
+        paint = np.zeros(paint.shape,np.uint8)
+        
+    #img[rect_y:rect_y+rect_height,rect_x:rect_x+rect_len] =  canvas
     #cv2.imshow('res', res)
     cv2.imshow('img', img)
     #cv2.imshow('img_blur1', img_blur1)
     cv2.imshow('canvas', canvas)
-    cv2.imshow('paint',paint)
+    #cv2.imshow('paint',paint)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
